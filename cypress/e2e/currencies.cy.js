0024be0aa2dca95e98d2870df416d67fb5d1be6a
Hubert Rozplochowski <hubert.rozplochowski@gmail.com>
2022-07-16 23:08:05 +0200

    A few tests added

--- cypress/e2e/currencies.cy.js
+++ cypress/e2e/currencies.cy.js
@@ -9,10 +9,11 @@ describe('Currencies App', () => {
     });
     cy.intercept('https://api.nbp.pl/api/exchangerates/tables/A/1700-07-01/?format=json', {
       fixture: 'error'
-    });
+    }).as('error');
     cy.get('.p-selectbutton .p-button').first().as('darkBtn');
     cy.get('.p-selectbutton .p-button').last().as('lightBtn');
     cy.get('table tbody tr').as('rows');
+    cy.get('p-calendar').as('calendar');
   });
 
   it('should have dark theme', () => {
@@ -37,5 +38,17 @@ describe('Currencies App', () => {
     cy.get('p-dropdown').as('dropdown').click();
     cy.get('@dropdown').find('p-dropdownitem').eq(1).click();
     cy.get('@rows').should('have.length', 10);
-  })
+  });
+
+  it('should display error notification after manually typing too old date', () => {
+    cy.get('@calendar').find('input').type('{selectall}{backspace}1700-07-01');
+    cy.wait('@error').then(() => {
+      cy.get('p-messages').children().should('have.length', 1);
+    });
+  });
+
+  it('should display date-picker', () => {
+    cy.get('@calendar').click();
+    cy.get('.p-datepicker').should('be.visible');
+  });
 });
