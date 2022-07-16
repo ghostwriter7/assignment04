8499e51fcebee2fa51a4bde1bc35b132e93a2d4e
Hubert Rozplochowski <hubert.rozplochowski@gmail.com>
2022-07-16 22:09:01 +0200

    Cypress installed, first tests added

---
+++ cypress/e2e/currencies.cy.js
@@ -0,0 +1,32 @@
+describe('Currencies App', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:4200');
+    cy.get('.p-selectbutton .p-button').first().as('darkBtn');
+    cy.get('.p-selectbutton .p-button').last().as('lightBtn');
+    cy.get('table tbody tr').as('rows');
+  });
+
+  it('should have dark theme', () => {
+    cy.get('@darkBtn').click();
+    cy.get('@darkBtn').should('have.class', 'p-highlight');
+    cy.get('@lightBtn').should('not.have.class', 'p-highlight');
+    cy.get('#app-theme').should('have.attr', 'href').and('eq', 'dark.css');
+  });
+
+  it('should have light theme', () => {
+    cy.get('@lightBtn').click();
+    cy.get('@lightBtn').should('have.class', 'p-highlight');
+    cy.get('@darkBtn').should('not.have.class', 'p-highlight');
+    cy.get('#app-theme').should('have.attr', 'href').and('eq', 'light.css');
+  });
+
+  it('should display 5 rows', () => {
+    cy.get('@rows').should('have.length', 5);
+  });
+
+  it('should display 10 rows', () => {
+    cy.get('p-dropdown').as('dropdown').click();
+    cy.get('@dropdown').find('p-dropdownitem').eq(1).click();
+    cy.get('@rows').should('have.length', 10);
+  })
+});
